5ef419014a03c8175aa3921d232c2e09ce77383b
liguanjian@rd.com <liguanjian@rd.com>
2017-05-19 10:19:07 +0800

    修改密码只需要旧密码

--- login.js
+++ login.js
@@ -21,7 +21,7 @@ function loginInit(app) {
                     res.send(JSON.stringify({code: -3, detail: '密码错误'}));
                     //todo 限制下次登录时间，不可以连续暴力刷新
                 } else {
-                    token.sign({name:req.query.name, pw:req.query.password}, function (t) {
+                    token.sign({name: req.query.name, pw: req.query.password}, function (t) {
                         res.send(JSON.stringify({code: 0, detail: '登录成功!', token: t}));
                     })
                 }
@@ -44,7 +44,7 @@ function loginInit(app) {
                 ds.insertUser(req.query.name, req.query.password, function () {
                     res.send(JSON.stringify({code: 0, detail: '注册成功!'}));
                 }, function (err) {
-                    res.send(JSON.stringify({code: 0, err:err}));
+                    res.send(JSON.stringify({code: 0, err: err}));
                 })
             }
         }, function (err) {
@@ -54,20 +54,32 @@ function loginInit(app) {
     });
 
     app.get('/ser/change', function (req, res) {
-        token.verifyToken(token, req, res, function (name) {
-            if (!req.query.password) {
-                res.send(JSON.stringify({code: -2, detail: '修改密码，需要密码'}));
-                return;
-            }
-            ds.changePasswd(name, req.query.password, function (results) {
-                if (results.affectedRows >= 1) {
-                    res.send(JSON.stringify({code: 0, detail: '修改密码成功!'}));
-                }else {
-                    res.send(JSON.stringify({code: -3, detail: '修改失败，检查用户名与密码!'}));
+        if (!req.query.password || !req.query.name || !req.query.oldpassword) {
+            res.send(JSON.stringify({code: -2, detail: '参数缺失'}));
+            return;
+        }
+        ds.queryUser(req.query.name, function (results) {
+            if (results.length < 1) {
+                res.send(JSON.stringify({code: -4, detail: '用户不存在'}));
+            } else {
+                var u = results[0];
+                if (req.query.password != u.password) {
+                    res.send(JSON.stringify({code: -3, detail: '密码错误'}));
+                    //todo 限制下次登录时间，不可以连续暴力刷新
+                } else {
+                    ds.changePasswd(req.query.name, req.query.password, function (results) {
+                        if (results.affectedRows >= 1) {
+                            res.send(JSON.stringify({code: 0, detail: '修改密码成功!'}));
+                        } else {
+                            res.send(JSON.stringify({code: -3, detail: '修改失败，检查用户名与密码!'}));
+                        }
+                    }, function (err) {
+                        res.send(JSON.stringify({code: -1, err: err}));
+                    })
                 }
-            }, function (err) {
-                res.send(JSON.stringify({code: -1, err: err}));
-            })
+            }
+        }, function (err) {
+            res.send(JSON.stringify({code: -1, err: err}));
         })
     });
 }
